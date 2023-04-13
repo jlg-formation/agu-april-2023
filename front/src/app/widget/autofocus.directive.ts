780836cf6283408fd868ee71643cdc9276f30ed6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-13 16:56:22 +0200

    dev: added input on the directive

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+export type AutofocusMode = 'focus' | 'select';
 
 @Directive({
   selector: 'input[appAutofocus]',
 })
 export class AutofocusDirective implements OnInit {
+  @Input()
+  appAutofocus: AutofocusMode = 'focus';
+
   constructor(private readonly elt: ElementRef<HTMLInputElement>) {}
   ngOnInit(): void {
-    console.log('this.elt.nativeElement: ', this.elt.nativeElement);
-    this.elt.nativeElement.select();
+    if (this.appAutofocus === 'select') {
+      this.elt.nativeElement.select();
+      return;
+    }
+    this.elt.nativeElement.focus();
   }
 }
