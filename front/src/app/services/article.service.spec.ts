fb8165b30c08e0120993bbdd77bc839530e0defd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-14 12:09:37 +0200

    dev: added coverage on service

--- front/src/app/services/article.service.spec.ts
+++ front/src/app/services/article.service.spec.ts
@@ -1,6 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
 
 import { ArticleService } from './article.service';
+import { newArticle } from 'src/test/article.data';
+import { catchError, of } from 'rxjs';
 
 describe('ArticleService', () => {
   let service: ArticleService;
@@ -13,4 +15,24 @@ describe('ArticleService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a new article', () => {
+    service.add(newArticle).subscribe();
+    expect(service).toBeTruthy();
+  });
+
+  it('should add a new article in error', fakeAsync(() => {
+    let shouldGoHere = false;
+    service
+      .add({ ...newArticle, name: 'xxx' })
+      .pipe(
+        catchError(() => {
+          shouldGoHere = true;
+          return of(undefined);
+        })
+      )
+      .subscribe();
+    expect(service).toBeTruthy();
+    expect(shouldGoHere).toBe(true);
+  }));
 });
---
+++ front/src/test/article.data.ts
@@ -0,0 +1,7 @@
+import { NewArticle } from 'src/app/interfaces/article';
+
+export const newArticle: NewArticle = {
+  name: 'Bidule',
+  price: 0.99,
+  qty: 123,
+};
