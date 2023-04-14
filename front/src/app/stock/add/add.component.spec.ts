6b7e029e323c948d462007fb943b0ac6500283d9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-14 14:38:58 +0200

    dev: added coverage on add component

--- front/src/app/stock/add/add.component.spec.ts
+++ front/src/app/stock/add/add.component.spec.ts
@@ -1,9 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
-import { AddComponent } from './add.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AddComponent } from './add.component';
+import { of, throwError } from 'rxjs';
 
 describe('AddComponent', () => {
   let component: AddComponent;
@@ -23,4 +30,50 @@ describe('AddComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should guide qty', fakeAsync(() => {
+    const inputEl = fixture.debugElement.query(
+      By.css('input[formcontrolname="qty"]')
+    ).nativeElement;
+    inputEl.value = '12';
+    inputEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  }));
+
+  it('should guide qty with null value', fakeAsync(() => {
+    component.f.controls.qty.setValue(null);
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  }));
+
+  it('should guide qty on bad value', fakeAsync(() => {
+    const inputEl = fixture.debugElement.query(
+      By.css('input[formcontrolname="qty"]')
+    ).nativeElement;
+    inputEl.value = '12x';
+    inputEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  }));
+
+  it('should submit', fakeAsync(() => {
+    component.submit();
+    tick(4000);
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  }));
+
+  it('should submit in error', fakeAsync(() => {
+    const refresh = spyOn(component['articleService'], 'add');
+    refresh.and.returnValue(
+      throwError(() => {
+        return new Error('oups');
+      })
+    );
+    component.submit();
+    tick(4000);
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  }));
 });
